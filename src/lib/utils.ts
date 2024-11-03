e2dbd08262d48bde9625f23de247b44fab69c48f
Bolori <109837376+Mhboolor@users.noreply.github.com>
2024-11-03 23:04:55 +0330

    updated eslint rules & implement cn function

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/** Combines and merges Tailwind CSS classes */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
