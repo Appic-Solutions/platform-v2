34e38e583347614e661c3af8b4a6f3d645870abe
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-11-06 16:09:34 +0330

    fix: add dark class to body to set dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className=" relative w-full max-w-[1920px] bg-background-dark">
+      <body className="dark relative w-full max-w-[1920px] bg-background-dark">
         <HeaderPage />
         {children}
       </body>
