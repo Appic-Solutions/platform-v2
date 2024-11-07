cd6a0dc3bbd214cf22d7a137c3ed39e4e545d55d
asjn3e <asjn3e@gmail.com>
2024-11-07 15:08:15 +0400

    add types

---
+++ src/blockchain_api/types/tokens.ts
@@ -0,0 +1,22 @@
+interface EvmToken {
+  name: string; // Name of the token (e.g., "Ethereum")
+  symbol: string; // Token symbol (e.g., "ETH")
+  logo: string; // URL for the token's logo image
+  contractAddress: string; // Address of the token's contract
+  usdPrice: number; // Current price of the token in USD
+  decimals: number; // Number of decimals
+  chainId: number; // ID of the blockchain (e.g., 1 for Ethereum mainnet)
+  holdingAmount?: number; // Optional: Amount the user holds of this token
+  usdHoldingAmount?: number; // Optional: Value in USD of the user's holdings
+}
+
+interface IpcToken {
+  name: string; // Name of the token (e.g., "Ethereum")
+  symbol: string; // Token symbol (e.g., "ETH")
+  logo: string; // URL for the token's logo image
+  canisterId: string; // Address of the token's contract
+  usdPrice: number; // Current price of the token in USD
+  decimals: number; // Number of decimals
+  holdingAmount?: number; // Optional: Amount the user holds of this token
+  usdHoldingAmount?: number; // Optional: Value in USD of the user's holdings
+}
