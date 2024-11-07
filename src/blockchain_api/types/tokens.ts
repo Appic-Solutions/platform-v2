493ab3745258c161206cd712753f5c80feb539eb
asjn3e <asjn3e@gmail.com>
2024-11-07 16:57:09 +0400

    add sample tokens to be used in token picker component

--- src/blockchain_api/types/tokens.ts
+++ src/blockchain_api/types/tokens.ts
@@ -1,22 +1,13 @@
-interface EvmToken {
+interface Token {
   name: string; // Name of the token (e.g., "Ethereum")
   symbol: string; // Token symbol (e.g., "ETH")
   logo: string; // URL for the token's logo image
-  contractAddress: string; // Address of the token's contract
+  contractAddress?: string; // Address of the token's contract if the token is an EVm token
+  canisterId?: string; // Address of the token's cansiter id if the token is an ICP token
   usdPrice: number; // Current price of the token in USD
   decimals: number; // Number of decimals
-  chainId: number; // ID of the blockchain (e.g., 1 for Ethereum mainnet)
+  chainId: number; // ID of the blockchain (e.g., 1 for Ethereum mainnet) if the blockchain is icp the token id is 0
   holdingAmount?: number; // Optional: Amount the user holds of this token
   usdHoldingAmount?: number; // Optional: Value in USD of the user's holdings
-}
-
-interface IcpToken {
-  name: string; // Name of the token (e.g., "Ethereum")
-  symbol: string; // Token symbol (e.g., "ETH")
-  logo: string; // URL for the token's logo image
-  canisterId: string; // Address of the token's contract
-  usdPrice: number; // Current price of the token in USD
-  decimals: number; // Number of decimals
-  holdingAmount?: number; // Optional: Amount the user holds of this token
-  usdHoldingAmount?: number; // Optional: Value in USD of the user's holdings
+  chainTypes: ChainType;
 }
