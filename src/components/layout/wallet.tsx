9632c62cece455acda472a0ed419556c86b16880
asjn3e <asjn3e@gmail.com>
2024-11-12 18:11:24 +0400

    Fix Evm wallet connection

--- src/components/layout/wallet.tsx
+++ src/components/layout/wallet.tsx
@@ -19,11 +19,7 @@ const WalletPage = () => {
   // const unauthenticatedAgent = useUnAuthenticatedAgent();
 
   // ICP Wallet Hooks
-  const {
-    identity: icpIdentity,
-    connect: connectIcp,
-    disconnect: disconnectIcp,
-  } = useIdentityKit();
+  const { identity: icpIdentity, connect: connectIcp, disconnect: disconnectIcp } = useIdentityKit();
 
   // Fetch All ICP Tokens
   // const fetchIcpBalance = async () => {
@@ -39,28 +35,22 @@ const WalletPage = () => {
   //   }
   // }, [unauthenticatedAgent]);
 
+  useEffect(() => {}, []);
 
   // EVM Wallet Hooks
   const { open: openEvmModal } = useAppKit();
   const { isConnected: isEvmConnected, address: evmAddress } = useAppKitAccount();
-  const { disconnect: disconnectEvm } = useDisconnect()
+  const { disconnect: disconnectEvm } = useDisconnect();
 
   useEffect(() => {
     console.log("evmAddress > ", evmAddress);
-  }, [evmAddress])
+  }, [evmAddress]);
 
   return (
-    <div className={cn(
-      "hidden lg:flex items-center justify-evenly gap-2 relative min-w-fit w-[165px] h-[42px]",
-      "rounded-round bg-[#faf7fd]/50 border border-[#ECE6F5]",
-      (icpIdentity || isEvmConnected) && "px-3",
-      "*:rounded-round"
-    )}>
+    <div className={cn("hidden lg:flex items-center justify-evenly gap-2 relative min-w-fit w-[165px] h-[42px]", "rounded-round bg-[#faf7fd]/50 border border-[#ECE6F5]", (icpIdentity || isEvmConnected) && "px-3", "*:rounded-round")}>
       {(!icpIdentity || !isEvmConnected) && (
         <Popover open={showPopover} onOpenChange={setShowPopover}>
-          <PopoverTrigger className="w-full text-black font-medium text-sm dark:text-white py-2 px-3">
-            + Add Wallet
-          </PopoverTrigger>
+          <PopoverTrigger className="w-full text-black font-medium text-sm dark:text-white py-2 px-3">+ Add Wallet</PopoverTrigger>
           <PopoverContent className="w-72 translate-y-4 flex flex-col gap-y-4" align="end">
             <div className="flex items-center justify-center text-black font-medium dark:text-white">
               <PopoverClose className="absolute top-4 right-4">
@@ -68,29 +58,16 @@ const WalletPage = () => {
               </PopoverClose>
               Select Wallet
             </div>
-            <div className={cn(
-              "flex flex-col  gap-4 text-black font-medium text-sm dark:text-white",
-              "*:flex *:items-center *:gap-2 *:cursor-pointer *:p-2 *:rounded-sm *:duration-200"
-            )}>
+            <div className={cn("flex flex-col  gap-4 text-black font-medium text-sm dark:text-white", "*:flex *:items-center *:gap-2 *:cursor-pointer *:p-2 *:rounded-sm *:duration-200")}>
               {!icpIdentity && (
                 <div onClick={() => connectIcp()} className="hover:bg-[#F5F5F5] dark:hover:bg-[#2A2A2A]">
-                  <Image
-                    src="/images/logo/icp-logo.png"
-                    alt="ICP Wallet"
-                    width={24}
-                    height={24}
-                  />
+                  <Image src="/images/logo/icp-logo.png" alt="ICP Wallet" width={24} height={24} />
                   Connect ICP Wallet
                 </div>
               )}
               {!isEvmConnected && (
                 <div onClick={() => openEvmModal()} className="hover:bg-[#F5F5F5] dark:hover:bg-[#2A2A2A]">
-                  <Image
-                    src="/images/logo/icp-logo.png"
-                    alt="EVM Wallet"
-                    width={24}
-                    height={24}
-                  />
+                  <Image src="/images/logo/icp-logo.png" alt="EVM Wallet" width={24} height={24} />
                   Connect EVM Wallet
                 </div>
               )}
@@ -101,12 +78,7 @@ const WalletPage = () => {
       {icpIdentity && (
         <Popover open={showIcpPopover} onOpenChange={setShowIcpPopover}>
           <PopoverTrigger>
-            <Image
-              src="/images/logo/icp-logo.png"
-              alt="ICP Wallet"
-              width={24}
-              height={24}
-            />
+            <Image src="/images/logo/icp-logo.png" alt="ICP Wallet" width={24} height={24} />
           </PopoverTrigger>
           <PopoverContent className="w-72 translate-y-4 flex flex-col gap-y-4" align="end">
             <div className="flex items-center justify-center text-black font-medium dark:text-white">
@@ -115,9 +87,7 @@ const WalletPage = () => {
               </PopoverClose>
               Your ICP Wallet
             </div>
-            <button
-              onClick={() => disconnectIcp()}
-              className="text-sm font-semibold text-fail px-4 py-2 rounded-m duration-200 hover:bg-fail hover:text-white">
+            <button onClick={() => disconnectIcp()} className="text-sm font-semibold text-fail px-4 py-2 rounded-m duration-200 hover:bg-fail hover:text-white">
               Disconnect
             </button>
           </PopoverContent>
@@ -126,12 +96,7 @@ const WalletPage = () => {
       {isEvmConnected && (
         <Popover open={showEvmPopover} onOpenChange={setShowEvmPopover}>
           <PopoverTrigger>
-            <Image
-              src="/images/logo/icp-logo.png"
-              alt="ICP Wallet"
-              width={24}
-              height={24}
-            />
+            <Image src="/images/logo/icp-logo.png" alt="ICP Wallet" width={24} height={24} />
           </PopoverTrigger>
           <PopoverContent className="w-72 translate-y-4 flex flex-col gap-y-4" align="end">
             <div className="flex items-center justify-center text-black font-medium dark:text-white">
@@ -140,18 +105,14 @@ const WalletPage = () => {
               </PopoverClose>
               Your EVM Wallet
             </div>
-            <div className="flex flex-col gap-4 text-black font-medium text-sm dark:text-white">
-              EVM DATA
-            </div>
-            <button
-              onClick={() => disconnectEvm()}
-              className="text-sm font-semibold text-fail px-4 py-2 rounded-m duration-200 hover:bg-fail hover:text-white">
+            <div className="flex flex-col gap-4 text-black font-medium text-sm dark:text-white">EVM DATA</div>
+            <button onClick={() => disconnectEvm()} className="text-sm font-semibold text-fail px-4 py-2 rounded-m duration-200 hover:bg-fail hover:text-white">
               Disconnect
             </button>
           </PopoverContent>
         </Popover>
       )}
-    </div >
+    </div>
   );
 };
 
