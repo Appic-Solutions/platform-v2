10022557b20989864b16b7e282f6ecb522c173f2
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2025-01-17 16:23:44 +0330

    fix bridge options

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -14,11 +14,7 @@ const BridgeHome = () => {
   const { setBridgePairs, setBridgeOptions } = useBridgeActions();
 
   const { data: bridgePairsData, isPending, isError } = useGetBridgePairs(unAuthenticatedAgent);
-  const {
-    mutateAsync: getBridgeOptions,
-    isPending: isPendingBridgeOptions,
-    isError: isErrorBridgeOptions,
-  } = useGetBridgeOptions();
+  const { mutateAsync: getBridgeOptions, isPending: isPendingBridgeOptions } = useGetBridgeOptions();
 
   useEffect(() => {
     if (bridgePairsData) setBridgePairs(bridgePairsData);
@@ -35,8 +31,11 @@ const BridgeHome = () => {
       };
       try {
         getBridgeOptions(getBridgeOptionsParams).then((res) => {
-          if (res.success) {
-            setBridgeOptions(res.result);
+          if (res) {
+            setBridgeOptions({
+              message: res.message,
+              options: res.result,
+            });
           }
         });
       } catch (error) {
@@ -48,12 +47,7 @@ const BridgeHome = () => {
   const renderStep = () => {
     switch (activeStep) {
       case 1:
-        return (
-          <BridgeSelectTokenPage
-            isErrorBridgeOptions={isErrorBridgeOptions}
-            isPendingBridgeOptions={isPendingBridgeOptions}
-          />
-        );
+        return <BridgeSelectTokenPage isPendingBridgeOptions={isPendingBridgeOptions} />;
       case 2:
         return <TokenListPage isPending={isPending} isError={isError} />;
       case 3:
