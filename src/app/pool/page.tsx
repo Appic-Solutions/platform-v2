d71b42172eb8a27547c7baf9cf70331ddbb0baf9
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2025-06-21 14:03:57 +0330

    feat: implement position detail page

--- src/app/pool/page.tsx
+++ src/app/pool/page.tsx
@@ -1,5 +1,6 @@
 import NewPosition from './_components/new-position';
+import PositionDetail from './_components/position-detail';
 
 export default function PoolPage() {
-  return <NewPosition />;
+  return <PositionDetail />;
 }
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -33,3 +33,4 @@ export { default as RefreshIcon } from './refresh';
 export { default as ZoomOutIcon } from './zoom-out';
 export { default as ZoomInIcon } from './zoom-in';
 export { default as ErrorIcon } from './error';
+export { default as ManageSearchIcon } from './manage-search';
