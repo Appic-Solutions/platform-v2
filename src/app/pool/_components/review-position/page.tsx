60188a1f48c400c33a4b8358c552055989c49b05
Bolori <109837376+Mhboolor@users.noreply.github.com>
2025-06-21 17:07:44 +0330

    feat: implement pool review page style

--- src/app/pool/_components/review-position/page.tsx
+++ src/app/pool/_components/review-position/page.tsx
@@ -9,12 +9,12 @@ export default function ReviewPositionPage() {
       className={cn(
         'gap-x-16 gap-y-10 md:flex-row',
         'md:w-[1204px]',
-        'md:p-12',
+        'md:px-12 md:py-9',
         'text-white md:text-black md:dark:text-white',
       )}
     >
-      <div>
-        <div className="flex w-full items-center justify-between gap-4">
+      <div className="max-w-[606px]">
+        <div className={cn('flex items-center justify-between gap-4', 'mb-10 w-full')}>
           <div className="flex items-center gap-x-2.5">
             <div className="relative flex">
               <Avatar
@@ -28,7 +28,7 @@ export default function ReviewPositionPage() {
                 className="-ml-4 h-[34px] w-[34px] md:h-[46px] md:w-[46px]"
               />
             </div>
-            <div className="text-[27px] font-bold md:text-[34px]">USDC/ETH</div>
+            <div className="text-[27px] font-bold md:text-[40px]">USDC/ETH</div>
           </div>
           <div className="flex items-center gap-x-1">
             <div
@@ -53,26 +53,27 @@ export default function ReviewPositionPage() {
         </div>
         <div
           className={cn(
-            'flex flex-col gap-y-2.5 md:gap-y-5',
+            'flex flex-col gap-y-2.5 md:gap-y-3.5',
             'bg-[#222222]',
             'rounded-[21px]',
             'px-6 py-5 md:px-8 md:py-6',
             'w-full',
+            'mb-10 md:mb-[55px]',
           )}
         >
-          <div className="flex">
+          <div className={cn('flex items-center gap-x-1.5', 'text-lg font-medium md:text-xl')}>
             <InfoCircleIcon />
             Create new pool
           </div>
-          <p>
+          <p className="text-sm text-white/70 md:text-[15px]">
             Choose the tokens you want to provide liquidity for. You can select tokens on all
             supported networks.. Choose the tokens you want to provide liquidity for. You can select
             tokens on all supported networks.
           </p>
         </div>
         <div>
-          <p>Set initial price</p>
-          <p>
+          <p className="mb-1.5 text-xl md:text-2xl">Set initial price</p>
+          <p className="mb-2.5 max-w-[528px] text-[13px] md:mb-[18px] md:text-[15px]">
             Choose the tokens you want to provide liquidity for. You can select tokens on all
             supported networks.
           </p>
@@ -83,12 +84,23 @@ export default function ReviewPositionPage() {
               'px-8 py-6',
               'rounded-[28px] backdrop-blur-[30px]',
               'border-2 border-[#4C4C4C]/30',
+              'mb-3 md:mb-5',
             )}
           >
-            <div className="flex items-center justify-between gap-4">
-              <p>initial price</p>
-              <div className="flex">
-                <div className="flex">
+            <div className={cn('flex items-center justify-between gap-4', 'mb-9 md:mb-10')}>
+              <p className="font-semibold text-white/70 md:text-[21px]">initial price</p>
+              <div
+                className={cn(
+                  'flex items-center justify-center gap-1',
+                  'bg-[#2B2B2B]',
+                  'rounded-[10px] p-1',
+                  '*:flex *:items-center *:justify-center *:gap-x-1',
+                  '*:px-2.5 *:py-1',
+                  '*:rounded-[6px]',
+                  'text-xs font-semibold',
+                )}
+              >
+                <div className={cn('bg-[#1E53B8]')}>
                   <Avatar
                     // src={token?.logo}
                     src="/images/logo/icp-logo.svg"
@@ -96,7 +108,7 @@ export default function ReviewPositionPage() {
                   />
                   USDC
                 </div>
-                <div className="flex">
+                <div className="">
                   <Avatar
                     // src={token?.logo}
                     src="/images/logo/icp-logo.svg"
@@ -106,29 +118,32 @@ export default function ReviewPositionPage() {
                 </div>
               </div>
             </div>
-            <div>
-              <p>1234.55</p>
-              <p>1 USDC = 1 ETH</p>
+            <div className="font-semibold">
+              <p className="text-[22px] md:text-[27px]">1234.55</p>
+              <p className="text-xs text-white/50 md:text-sm">1 USDC = 1 ETH</p>
             </div>
           </div>
         </div>
         <div
           className={cn(
-            'flex flex-col gap-y-2.5 md:flex-row md:gap-y-5',
+            'flex flex-col gap-y-2.5 md:flex-row md:justify-between md:gap-y-5',
             'bg-[#222222]',
             'rounded-[21px]',
             'px-6 py-5 md:px-8 md:py-6',
             'w-full',
+            '*:text-sm *:md:text-[17px]',
           )}
         >
-          <p>Market price: 0 ICP = 1 ETH (-)</p>
-          <p>Use market price</p>
+          <p>
+            <span className="text-white/80">Market price: </span>0 ICP = 1 ETH (-)
+          </p>
+          <p className="text-white/80">Use market price</p>
         </div>
       </div>
       <div>
-        <div>
-          <p>Set price range</p>
-          <div className="flex">
+        <div className="mb-10 md:mb-[55px]">
+          <p className="mb-3 text-xl md:mb-4 md:text-2xl">Set price range</p>
+          <div className="flex w-full items-center gap-2 *:w-full md:gap-5">
             <div
               className={cn(
                 'relative isolate',
@@ -138,10 +153,10 @@ export default function ReviewPositionPage() {
                 'border-2 border-[#4C4C4C]/30',
               )}
             >
-              <p>Min price</p>
-              <div>
-                <p>1234.55</p>
-                <p>USDC = 1 ETH</p>
+              <p className="mb-10 text-white/70 md:text-[21px]">Min price</p>
+              <div className="font-semibold">
+                <p className="text-[22px] md:text-[27px]">1234.55</p>
+                <p className="text-xs text-white/50 md:text-sm">USDC = 1 ETH</p>
               </div>
             </div>
             <div
@@ -153,21 +168,21 @@ export default function ReviewPositionPage() {
                 'border-2 border-[#4C4C4C]/30',
               )}
             >
-              <p>Max price</p>
-              <div>
-                <p>1234.55</p>
-                <p>USDC = 1 ETH</p>
+              <p className="mb-10 text-white/70 md:text-[21px]">Max price</p>
+              <div className="font-semibold">
+                <p className="text-[22px] md:text-[27px]">1234.55</p>
+                <p className="text-xs text-white/50 md:text-sm">USDC = 1 ETH</p>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <p>Deposit tokens</p>
-          <p>
+        <div className="mb-10 md:mb-[34px]">
+          <p className="mb-2 text-xl md:mb-2.5 md:text-2xl">Deposit tokens</p>
+          <p className="mb-3 text-sm text-white/70 md:mb-5 md:text-[15px]">
             The amount earned providing liquidity. Choose an amount that suits your risk tolerance
             and strategy.
           </p>
-          <div className="flex">
+          <div className="flex w-full items-center gap-2 *:w-full md:gap-5">
             <div
               className={cn(
                 'relative isolate',
@@ -177,7 +192,9 @@ export default function ReviewPositionPage() {
                 'border-2 border-[#4C4C4C]/30',
               )}
             >
-              <div className="flex">
+              <div
+                className={cn('flex items-center gap-1.5', 'font-semibold md:text-[21px]', 'mb-10')}
+              >
                 <Avatar
                   // src={token?.logo}
                   src="/images/logo/icp-logo.svg"
@@ -185,9 +202,9 @@ export default function ReviewPositionPage() {
                 />
                 USDC
               </div>
-              <div>
-                <p>1234.55</p>
-                <p>USDC = 1 ETH</p>
+              <div className="font-semibold">
+                <p className="text-[22px] md:text-[27px]">1234.55</p>
+                <p className="text-xs text-white/50 md:text-sm">USDC = 1 ETH</p>
               </div>
             </div>
             <div
@@ -199,7 +216,9 @@ export default function ReviewPositionPage() {
                 'border-2 border-[#4C4C4C]/30',
               )}
             >
-              <div className="flex">
+              <div
+                className={cn('flex items-center gap-1.5', 'font-semibold md:text-[21px]', 'mb-10')}
+              >
                 <Avatar
                   // src={token?.logo}
                   src="/images/logo/icp-logo.svg"
@@ -207,9 +226,9 @@ export default function ReviewPositionPage() {
                 />
                 USDC
               </div>
-              <div>
-                <p>1234.55</p>
-                <p>USDC = 1 ETH</p>
+              <div className="font-semibold">
+                <p className="text-[22px] md:text-[27px]">1234.55</p>
+                <p className="text-xs text-white/50 md:text-sm">USDC = 1 ETH</p>
               </div>
             </div>
           </div>
