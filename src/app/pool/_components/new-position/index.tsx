3fe3d872cd1ff4fb8b057a202a388cad40c688c4
Kourosh Eydivandi <kouroshey@gmail.com>
2025-06-29 10:18:39 +0330

    feat: create pool details page

--- src/app/pool/_components/new-position/index.tsx
+++ src/app/pool/_components/new-position/index.tsx
@@ -15,35 +15,35 @@ const NewPosition = () => {
 
   return (
     <div className="relative w-full">
-      <div
-        className={cn(
-          'hidden items-center justify-between gap-1.5 lg:flex',
-          'rounded-full bg-box-background text-white ring-[5px] ring-box-border',
-          'absolute -left-24 top-1/2 h-[185px] -translate-y-1/2 flex-col p-2',
-        )}
-      >
-        {['1', '2'].map((item) => (
-          <div
-            onClick={() => setStep(item)}
-            key={item}
-            className={cn(
-              'flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full transition-all',
-              step === item
-                ? 'bg-primary-buttons'
-                : 'bg-[linear-gradient(81.4deg,rgba(239,239,239,0)-15.41%,rgba(164,164,164,0.24)113.98%)]',
-            )}
-          >
-            {item}
-          </div>
-        ))}
-        <div className="absolute top-1/2 h-[53px] w-[3px] -translate-y-1/2 rounded-full bg-[rgba(86,144,255,1)]"></div>
-      </div>
       <Box
         className={cn(
-          'text-white transition-all md:p-12 lg:text-black lg:dark:text-white',
+          'text-white transition-all md:p-12 lg:overflow-visible lg:text-black lg:dark:text-white',
           step === '2' ? 'md:h-[789px] lg:w-[1204px]' : 'lg:w-[611px]',
         )}
       >
+        <div
+          className={cn(
+            'hidden items-center justify-between gap-1.5 lg:flex',
+            'rounded-full bg-box-background text-white ring-[5px] ring-box-border',
+            'absolute -left-24 top-1/2 h-[185px] -translate-y-1/2 flex-col p-2',
+          )}
+        >
+          {['1', '2'].map((item) => (
+            <div
+              onClick={() => setStep(item)}
+              key={item}
+              className={cn(
+                'flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full transition-all',
+                step === item
+                  ? 'bg-primary-buttons'
+                  : 'bg-[linear-gradient(81.4deg,rgba(239,239,239,0)-15.41%,rgba(164,164,164,0.24)113.98%)]',
+              )}
+            >
+              {item}
+            </div>
+          ))}
+          <div className="absolute top-1/2 h-[53px] w-[3px] -translate-y-1/2 rounded-full bg-[rgba(86,144,255,1)]"></div>
+        </div>
         {content()}
       </Box>
     </div>
