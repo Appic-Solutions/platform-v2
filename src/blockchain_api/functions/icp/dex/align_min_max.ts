e835c8976f490bc3358cba2447a85ed205d9d68c
Jeff <jeff@JEFFs-MacBook-Air.local>
2025-07-29 01:15:07 +0400

    fix: add support for min max price for align price function

--- src/blockchain_api/functions/icp/dex/align_min_max.ts
+++ src/blockchain_api/functions/icp/dex/align_min_max.ts
@@ -8,15 +8,18 @@ export interface AlignMinMaxPriceArgs {
 	is_token0_selected: boolean;
 	token0: IcpToken;
 	token1: IcpToken;
-	price: string;
+	min_price: string;
+	max_price: string;
 	tick_spacing: number;
 	is_min_price: boolean;
 	pool_sqrt_x98_price: string;
 }
 
 export interface AlignedPrice {
-	tick: number;
-	price: string;
+	min_tick: number;
+	max_tick: number;
+	min_price: string;
+	max_price: string;
 	is_token0_active: boolean,
 	is_token1_active: boolean,
 }
@@ -24,16 +27,23 @@ export function alignMinOrMaxPrice({
 	is_token0_selected,
 	token0,
 	token1,
-	price,
+	min_price,
+	max_price,
 	tick_spacing,
-	is_min_price,
+
 	pool_sqrt_x98_price,
 }: AlignMinMaxPriceArgs): AlignedPrice {
 	// Input validation
 
-	if (!price || Number(price) <= 0) {
+	if (!min_price || Number(min_price) <= 0) {
 		throw new Error('Invalid price input');
 	}
+	if (!max_price || Number(max_price) <= 0) {
+		throw new Error('Invalid price input');
+	}
+
+
+
 	if (tick_spacing <= 0) {
 		throw new Error('Tick spacing must be positive');
 	}
@@ -41,69 +51,107 @@ export function alignMinOrMaxPrice({
 		throw new Error('Tokens must be provided');
 	}
 
-	let tick: number;
+	let min_tick: number;
+	let max_tick: number;
 
-	if (price == 'min') {
-		tick = is_token0_selected ? -887272 : 887272;
-	} else if (price == 'max') {
-		tick = is_token0_selected ? 887272 : -887272;
+
+	if (min_price == 'min') {
+		min_tick = is_token0_selected ? -887272 : 887272;
+	} else if (min_price == 'max') {
+		min_tick = is_token0_selected ? 887272 : -887272;
 	} else {
 		// Calculate sqrt price from input
 		const sqrtRatioX96 = new BigNumber(
-			calculate_price({ token0, token1, price, is_token0_selected }).sqrt_price_x96,
+			calculate_price({ token0, token1, price: min_price, is_token0_selected }).sqrt_price_x96,
 		);
 
 
 
 		// Get corresponding tick
-		tick = TickMath.getTickAtSqrtRatio(BigInt(sqrtRatioX96.toFixed()));
+		min_tick = TickMath.getTickAtSqrtRatio(BigInt(sqrtRatioX96.toFixed()));
 	}
 
-	console.log("TICK NOT ALIGNED", tick);
+	if (max_price == 'min') {
+		max_tick = is_token0_selected ? -887272 : 887272;
+	} else if (max_price == 'max') {
+		max_tick = is_token0_selected ? 887272 : -887272;
+	} else {
+		// Calculate sqrt price from input
+		const sqrtRatioX96 = new BigNumber(
+			calculate_price({ token0, token1, price: max_price, is_token0_selected }).sqrt_price_x96,
+		);
+
+
+
+		// Get corresponding tick
+		max_tick = TickMath.getTickAtSqrtRatio(BigInt(sqrtRatioX96.toFixed()));
+	}
+
+
+	console.log("TICK NOT ALIGNED", min_tick, max_tick);
 
 	// Find closest aligned tick
-	let tickAligned = Math.round(tick / tick_spacing) * tick_spacing;
+	let min_tickAligned = Math.round(min_tick / tick_spacing) * tick_spacing;
+	let max_tickAligned = Math.round(max_tick / tick_spacing) * tick_spacing;
 
 
 
-	if (tickAligned < -887272) {
-		tickAligned += tick_spacing;
-	} else if (tickAligned > 887272) {
-		tickAligned -= tick_spacing
+
+	if (min_tickAligned < -887272) {
+		min_tickAligned += tick_spacing;
+	} else if (min_tickAligned > 887272) {
+		min_tickAligned -= tick_spacing
+	}
+
+	if (max_tickAligned < -887272) {
+		max_tickAligned += tick_spacing;
+	} else if (max_tickAligned > 887272) {
+		max_tickAligned -= tick_spacing
 	}
 
 
-	console.log("Tick HELLOOOO", tickAligned);
+
+	console.log("Tick HELLOOOO", min_tickAligned, max_tickAligned);
 
 
 	// Convert back to sqrt price
-	const sqrtPrice = new BigNumber(TickMath.getSqrtRatioAtTick(tickAligned).toString());
+	const min_sqrtPrice = new BigNumber(TickMath.getSqrtRatioAtTick(min_tickAligned).toString());
+	const max_sqrtPrice = new BigNumber(TickMath.getSqrtRatioAtTick(max_tickAligned).toString());
 
 
 
 	// Calculate price in token1 terms
-	const priceInToken1 = sqrtPrice
+	const min_priceInToken1 = min_sqrtPrice
 		.div(Q96)
 		.pow(2)
 		.div(new BigNumber(10).pow(token1.decimals - token0.decimals));
 
+	const max_priceInToken1 = max_sqrtPrice
+		.div(Q96)
+		.pow(2)
+		.div(new BigNumber(10).pow(token1.decimals - token0.decimals));
+
+
+
 	let is_token0_active = true;
 	let is_token1_active = true;
-	let is_min_price_slected = is_token0_selected ? is_min_price : !is_min_price;
 
-	if (is_min_price_slected && BigNumber(pool_sqrt_x98_price).lt(sqrtPrice)) {
+	const price_lower = min_sqrtPrice.lt(max_sqrtPrice) ? min_sqrtPrice : max_sqrtPrice;
+	const price_higher = min_sqrtPrice.lt(max_sqrtPrice) ? max_sqrtPrice : min_sqrtPrice;
+
+
+	if (BigNumber(pool_sqrt_x98_price).lt(price_lower)) {
 		is_token1_active = false;
 	}
 
-	if (!is_min_price_slected && BigNumber(pool_sqrt_x98_price).gt(sqrtPrice)) {
+	if (BigNumber(pool_sqrt_x98_price).gt(price_higher)) {
 		is_token0_active = false;
 	}
 
 
 
-
 	// Return price based on token selection
 	return is_token0_selected
-		? { price: priceInToken1.toString(), tick: tickAligned, is_token0_active, is_token1_active }
-		: { price: new BigNumber(1).div(priceInToken1).toString(), tick: tickAligned, is_token0_active, is_token1_active };
+		? { min_price: min_priceInToken1.toString(), max_price: max_priceInToken1.toString(), min_tick: min_tickAligned, max_tick: max_tickAligned, is_token0_active, is_token1_active }
+		: { min_price: new BigNumber(1).div(min_priceInToken1).toString(), max_price: new BigNumber(1).div(min_priceInToken1).toString(), max_tick: max_tickAligned, min_tick: min_tickAligned, is_token0_active, is_token1_active };
 }
