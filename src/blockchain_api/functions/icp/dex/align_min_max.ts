3b69e991b847568803f2273e0ef27696db55f5e0
Jeff <jeff@JEFFs-MacBook-Air.local>
2025-07-28 17:25:21 +0400

    fix: add is_token0_active and is_token1_active at the time of position creation

--- src/blockchain_api/functions/icp/dex/align_min_max.ts
+++ src/blockchain_api/functions/icp/dex/align_min_max.ts
@@ -10,11 +10,15 @@ export interface AlignMinMaxPriceArgs {
 	token1: IcpToken;
 	price: string;
 	tick_spacing: number;
+	is_min_price: boolean;
+	pool_sqrt_x98_price: string;
 }
 
 export interface AlignedPrice {
 	tick: number;
 	price: string;
+	is_token0_active: boolean,
+	is_token1_active: boolean,
 }
 export function alignMinOrMaxPrice({
 	is_token0_selected,
@@ -22,6 +26,8 @@ export function alignMinOrMaxPrice({
 	token1,
 	price,
 	tick_spacing,
+	is_min_price,
+	pool_sqrt_x98_price,
 }: AlignMinMaxPriceArgs): AlignedPrice {
 	// Input validation
 
@@ -73,14 +79,31 @@ export function alignMinOrMaxPrice({
 	// Convert back to sqrt price
 	const sqrtPrice = new BigNumber(TickMath.getSqrtRatioAtTick(tickAligned).toString());
 
+
+
 	// Calculate price in token1 terms
 	const priceInToken1 = sqrtPrice
 		.div(Q96)
 		.pow(2)
 		.div(new BigNumber(10).pow(token1.decimals - token0.decimals));
 
+	let is_token0_active = true;
+	let is_token1_active = true;
+	let is_min_price_slected = is_token0_selected ? is_min_price : !is_min_price;
+
+	if (is_min_price_slected && BigNumber(pool_sqrt_x98_price).lt(sqrtPrice)) {
+		is_token1_active = false;
+	}
+
+	if (!is_min_price_slected && BigNumber(pool_sqrt_x98_price).gt(sqrtPrice)) {
+		is_token0_active = false;
+	}
+
+
+
+
 	// Return price based on token selection
 	return is_token0_selected
-		? { price: priceInToken1.toString(), tick: tickAligned }
-		: { price: new BigNumber(1).div(priceInToken1).toString(), tick: tickAligned };
+		? { price: priceInToken1.toString(), tick: tickAligned, is_token0_active, is_token1_active }
+		: { price: new BigNumber(1).div(priceInToken1).toString(), tick: tickAligned, is_token0_active, is_token1_active };
 }
